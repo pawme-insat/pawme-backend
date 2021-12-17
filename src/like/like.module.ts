09855c87e942fc5bbf496f78e2db2814e40908fc
CyrineZaouali <cyrine.zaouali@insat.u-carthage.tn>
2021-12-17 18:24:05 +0100

    fix incorrect import

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { LikeService } from './like.service';
 import { LikeResolver } from './like.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Like } from 'typeorm';
+import { Like } from './entities/like.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Like])],
